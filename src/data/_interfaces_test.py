39e1a7d307a9253ab87db8392ede465585c89186
Anthony Vinicius Muller <anthonyvmuller@outlook.com>
2022-06-03 04:00:11 -0300

    feat: adding test to interfaces implementation

---
+++ src/data/_interfaces_test.py
@@ -0,0 +1,41 @@
+from src.core.models.mock_restaurant import create_mock_restaurant
+from .add_restaurant import AddRestaurant
+from src.persistent.MySQL.repositories.restaurant_repository import RestaurantRepository
+from .get_restaurant import GetRestaurant
+from faker import Faker as Mocker
+
+mocker = Mocker()
+
+def test_add_restaurant():
+    add_restaurant = AddRestaurant(RestaurantRepository())
+    restaurant_repository = RestaurantRepository()
+
+    restaurant = create_mock_restaurant()
+
+    id = RestaurantRepository.insert_restaurant(restaurant = restaurant) 
+    repository_restaurant = restaurant_repository.get_restaurant_by_id(id)
+    del repository_restaurant['id'] 
+
+    use_case_restaurant = add_restaurant.add(restaurant)   
+    del use_case_restaurant['data']['id'] 
+
+    RestaurantRepository.delete_restaurant_by_id(id)
+
+    assert use_case_restaurant['data'] == repository_restaurant
+    assert use_case_restaurant['success'] == True
+
+def test_get_restaurant():
+    get_restaurant = GetRestaurant(RestaurantRepository())
+    restaurant_repository = RestaurantRepository()
+
+    restaurant = create_mock_restaurant()
+
+    id = RestaurantRepository.insert_restaurant(restaurant) 
+
+    use_case_restaurant = get_restaurant.by_id(id)    
+    repository_restaurant = restaurant_repository.get_restaurant_by_id(id)
+
+    RestaurantRepository.delete_restaurant_by_id(id)
+
+    assert use_case_restaurant['data'] == repository_restaurant
+    assert use_case_restaurant['success'] == True
