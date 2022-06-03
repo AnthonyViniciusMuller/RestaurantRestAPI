26c4f59c0c0dfdc4e27927fd6fefbc390a9198a3
Anthony Vinicius Muller <anthonyvmuller@outlook.com>
2022-06-03 04:08:06 -0300

    feat: adding controllers

---
+++ src/presenters/controllers/delete_restaurant_controller.py
@@ -0,0 +1,35 @@
+from src.application.interfaces.route import RouteInterface
+from src.core.use_cases import DeleteRestaurant
+from src.presenters.helpers.http_request import HttpRequest
+from src.presenters.helpers.http_response import HttpResponse
+from typing import Type
+
+
+class DeleteRestaurantController(RouteInterface):
+
+    def __init__(self, use_case: Type[DeleteRestaurant]):
+        self.delete_restaurant = use_case
+
+    def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
+        
+        if http_request.params:
+
+            params = http_request.params
+
+            if "id" in params.keys() and isinstance(params['id'], int):
+                response = self.delete_restaurant.by_id(id = params['id'])
+                
+                if response['success'] == True:
+                    return HttpResponse(code = 200, body = response["data"])
+
+                return HttpResponse(code = 400, body = response["erro"])
+        
+        else:
+            response = self.delete_restaurant.all()
+                
+            if response['success'] == True:
+                return HttpResponse(code = 200, body = response["data"])
+
+            return HttpResponse(code = 400, body = response["erro"])
+
+        return HttpResponse(code = 400, body = "Bad Request")
---
+++ src/presenters/controllers/update_restaurant_controller.py
@@ -0,0 +1,58 @@
+from typing import Type
+from src.application.interfaces.route import RouteInterface
+from src.core.models.category import Category
+from src.core.models.restaurant import Restaurant
+from src.core.models.website import WebSite
+from src.core.use_cases.update_restaurant import UpdateRestaurant
+from src.presenters.helpers.http_request import HttpRequest
+from src.presenters.helpers.http_response import HttpResponse
+
+
+class UpdateRestaurantController(RouteInterface):
+
+    def __init__(self, use_case: Type[UpdateRestaurant]):
+        self.update_restaurant = use_case
+
+    def route(self, http_request: Type[HttpRequest]) -> HttpResponse:
+
+        if http_request.body:
+
+            params = http_request.params
+
+            body = http_request.body
+
+            if ('name' and 'postal_code' in body.keys()) and 'id' in params.keys() and isinstance(params['id'], int):
+
+                if("categories" in body.keys() and len(body["categories"]) > 0):
+                    categories = body["categories"] 
+                else:
+                    categories = [],
+
+                if("websites" in body.keys() and len(body["websites"]) > 0):
+                    websites = body["websites"] 
+                else:
+                    websites = [],
+                
+                restaurant = Restaurant(
+                    name = body["name"],
+                    postal_code = body["postal_code"],
+                    address = body["address"] if "address" in body.keys() else "",
+                    country = body["country"] if "country" in body.keys() else "",
+                    province = body["province"] if "province" in body.keys() else "",
+                    city = body["city"] if "city" in body.keys() else "",
+                    latitude = body["latitude"] if "latitude" in body.keys() else 0,
+                    longitude = body["longitude"] if "longitude" in body.keys() else 0,
+                    categories = [Category(name = category) for category in categories],
+                    websites = [WebSite(url = website) for website in websites]
+                )
+
+                response = self.update_restaurant.by_id(id, restaurant)
+                
+                if response['success'] == True:
+                    return HttpResponse(code = 200, body = response["data"])
+
+                return HttpResponse(code = 400, body = response["erro"])
+
+            return HttpResponse(code = 404, body = 'Bad Request')
+
+        return HttpResponse(code = 402, body = "Bad Request")
